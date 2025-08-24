752c4d2f6407a3db0d70b8f572574e99bb84c482
codeman4033 <92231243+codeman4033@users.noreply.github.com>
2025-08-24 18:03:44 -0500

    Update sass.ts

--- tools/plugins/sass.ts
+++ tools/plugins/sass.ts
@@ -33,24 +33,5 @@ export function sassPlugin(mod: string) {
           .replace("{PATH}", resultPath),
       };
     },
-    // setup(build) {
-    //   let cache = new Map();
-    //   build.onLoad({ filter: /\.s[ca]ss$/ }, async (args) => {
-    //     let input = await readFile(args.path, "utf8");
-    //     let key = args.path;
-    //     let value = cache.get(key);
-
-    //     if (!value || value.input !== input) {
-    //       let contents = await compileStringAsync(input);
-    //       value = { input, output: { contents } };
-    //       cache.set(key, value);
-    //     }
-
-    //     return {
-    //       contents: value.output,
-    //       loader: "css",
-    //     };
-    //   });
-    // },
   });
 }
